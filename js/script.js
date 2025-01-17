00aaf1ad991bd5e460a7bece3b5099669786af5a
Marilia Merendi <marilia.merendi@studio.unibo.it>
2025-01-17 19:18:59 +0100

    Added css and js script to toggle between mobile/desktop mode.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-button").addEventListener("click", function() {
+    const mobileMenu = document.querySelector(".mobile-menu");
+    mobileMenu.classList.toggle("hidden");
+});

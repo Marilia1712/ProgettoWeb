535d5f36706c2b6ba034c768f5c6639ec1d09069
Marilia Merendi <marilia.merendi@studio.unibo.it>
2025-01-22 23:57:35 +0100

    carousel base remake

--- template/lista-categorie.php
+++ template/lista-categorie.php
@@ -1,6 +1,6 @@
 <section>
     <div class="row">
-        <?php foreach($templateParams["categorie"] as $categoria): ?>
+        <?php foreach ($templateParams["categorie"] as $categoria): ?>
             <div class="col-12 col-md-6 mb-4">
                 <div class="card h-100">
                     <img src="<?php echo UPLOAD_DIR."categories/".$categoria["Immagine"]; ?>" class="card-img-top" alt="<?php echo $categoria["Nome"]; ?>">
@@ -18,41 +18,67 @@
     </div>
 </section>
 
-<!-- Sezione del carosello di immagini -->
-<script src="./js/script.js"></script>
-<section class="banner-categorie">
-    <div class="carousel">
-        <a href="./index-prodotti-categoria.php?nomeCategoria=Filati">
-            <img src="./upload/carousel/carousel1.jpg" alt="" class="carousel-image">
-            <p>Filati</p>
-        </a>
-        <a href="./index-prodotti-categoria.php?nomeCategoria=Tutto Borse">
-            <img src="./upload/carousel/carousel2.jpg" alt="" class="carousel-image">
-            <p>Borse</p>
-        </a>
-        <a href="./index-prodotti-categoria.php?nomeCategoria=Attrezzi">
-            <img src="./upload/carousel/carousel3.jpg" alt="" class="carousel-image">
-            <p>Attrezzi</p>
-        </a>
-        <a href="./index-prodotti-categoria.php?nomeCategoria=Bijoux">
-            <img src="./upload/carousel/carousel4.jpg" alt="" class="carousel-image">
-            <p>Bijoux</p>
-        </a>
+<!-- Carousel Section -->
+<section class="banner-categorie my-5">
+    <div id="categoryCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <a href="./index-prodotti-categoria.php?nomeCategoria=Filati">
+                    <img src="./upload/carousel/carousel1.jpg" class="d-block w-100" alt="Filati">
+                    <div class="carousel-caption">
+                        <p>Filati</p>
+                    </div>
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="./index-prodotti-categoria.php?nomeCategoria=Tutto Borse">
+                    <img src="./upload/carousel/carousel2.jpg" class="d-block w-100" alt="Borse">
+                    <div class="carousel-caption">
+                        <p>Borse</p>
+                    </div>
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="./index-prodotti-categoria.php?nomeCategoria=Attrezzi">
+                    <img src="./upload/carousel/carousel3.jpg" class="d-block w-100" alt="Attrezzi">
+                    <div class="carousel-caption">
+                        <p>Attrezzi</p>
+                    </div>
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="./index-prodotti-categoria.php?nomeCategoria=Bijoux">
+                    <img src="./upload/carousel/carousel4.jpg" class="d-block w-100" alt="Bijoux">
+                    <div class="carousel-caption">
+                        <p>Bijoux</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#categoryCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#categoryCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-dots"></div>
 </section>
 
-<!-- Sezione descrittiva del brand -->
-<section class="white-banner">
-    <div class="container">
-        <div class="content-wrapper">
-            <img src="./upload/logos/LOGO2.png" alt="All You Knit Logo" class="logo">
-            <p>
-                All You Knit, da oltre 30 anni, è un brand retrò che combina la passione per la maglieria con lo stile groovy
-                degli anni passati. I nostri filati e gomitoli, selezionati con cura, garantiscono morbidezza, resistenza e
-                colori vivaci che richiamano l'energia e la creatività di un’epoca unica. Coniughiamo tradizione e design
-                vintage per offrire materiali di alta qualità, perfetti per dare vita a progetti originali e duraturi.
-            </p>
+<!-- Brand Banner -->
+<section class="white-banner py-5">
+    <div class="container text-center">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10">
+                <img src="./upload/logos/LOGO2.png" alt="All You Knit Logo" class="img-fluid mb-4">
+                <p>
+                    All You Knit, da oltre 30 anni, è un brand retrò che combina la passione per la maglieria con lo stile groovy
+                    degli anni passati. I nostri filati e gomitoli, selezionati con cura, garantiscono morbidezza, resistenza e
+                    colori vivaci che richiamano l'energia e la creatività di un’epoca unica. Coniughiamo tradizione e design
+                    vintage per offrire materiali di alta qualità, perfetti per dare vita a progetti originali e duraturi.
+                </p>
+            </div>
         </div>
     </div>
 </section>

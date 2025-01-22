eee472238cb3619ac1093442e0537773876c8192
Marilia Merendi <marilia.merendi@studio.unibo.it>
2025-01-22 12:51:25 +0100

    Added boostrap grid system to base.php. Edited style.css.

--- template/base.php
+++ template/base.php
@@ -8,96 +8,98 @@
     <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
-
 </head>
 
 <body>
-    <!-- Header con logo e barra di ricerca -->
-    <header class="mainheader">
-        <form class="search-bar" action="#" method="get">
-            <input type="text" placeholder="Cerca un prodotto">
-            <button type="submit">🔍</button>                                       <!--fix me: sostituisci con vettoriale lente-->
-        </form>
-
-        <div class="logo">
-            <img src="./upload/logos/LOGO2.png" alt="Logo All You Knit">
+    <!-- Header -->
+    <header class="mainheader py-3">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Search Bar -->
+                <div class="col-lg-4 col-md-6">
+                    <form class="d-flex search-bar" action="#" method="get">
+                        <input type="text" class="form-control me-2" placeholder="Cerca un prodotto">
+                        <button type="submit" class="btn btn-primary">🔍</button>
+                    </form>
+                </div>
+                
+                <!-- Logo -->
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="logo">
+                        <img src="./upload/logos/LOGO2.png" alt="Logo All You Knit" class="img-fluid">
+                    </div>
+                </div>
+                
+                <!-- Icons -->
+                <div class="col-lg-4 text-end">
+                    <div class="loginbanner mb-2">
+                        <a href="./login.html" class="text-decoration-none">Ciao, Maurizio Capuano</a>
+                    </div>
+                    <div class="icons d-inline-flex gap-2">
+                        <a href="./notifications.html">
+                            <img src="./upload/icons/bell-solid.svg" alt="Notifiche" width="24">
+                        </a>
+                        <a href="./cart.html">
+                            <img src="./upload/icons/cart-shopping-solid.svg" alt="Carrello" width="24">
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="loginbanner">
-            <a href="./login.html" class="desktop-only">Ciao, Maurizio Capuano</a>
-        </div>
-        
-        <div class="icons">
-            <a href="./notifications.html">
-                <img src="./upload/icons/bell-solid.svg" alt="Notifiche">
-            </a>
-            <a href="./cart.html">
-                <img src="./upload/icons/cart-shopping-solid.svg" alt="Carrello">
-            </a>
-        </div>
-
-        <!-- Menu Hamburger -->
-        <div class="hamburger-menu">
-            <button id="menu-button">
-                ☰                                                  <!-- fixme: <img src="../upload/icons/bars-solid.svg" alt="Menu"> -->
-            </button>
-        </div>
-
     </header>
 
-    <!-- Barra di navigazione -->
-    <nav class="desktop-navbar">
-        <ul class="desktop-menu">
-            <li><a href="./index.php">Home</a></li>
-            <?php foreach($templateParams["categorie"] as $categoria): ?>
-            <li>
-                <a href="./index-prodotti-categoria.php?nomeCategoria=<?php echo $categoria["Nome"]?>"><?php echo $categoria["Nome"]; ?></a>
-            </li>
-            <?php endforeach; ?>
-            <li><a href="./index-offerte.php">Offerte</a></li>
-        </ul>
-
-        <div class="mobile-greeting mobile-only">
-            <p>Ciao, Maurizio Capuano</p>
-            <a href="#">Esci</a>
+    <!-- Navigation Bar -->
+    <nav class="desktop-navbar bg-light py-2">
+        <div class="container">
+            <ul class="nav justify-content-center">
+                <li class="nav-item"><a class="nav-link" href="./index.php">Home</a></li>
+                <?php foreach($templateParams["categorie"] as $categoria): ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="./index-prodotti-categoria.php?nomeCategoria=<?php echo $categoria["Nome"]?>">
+                        <?php echo $categoria["Nome"]; ?>
+                    </a>
+                </li>
+                <?php endforeach; ?>
+                <li class="nav-item"><a class="nav-link" href="./index-offerte.php">Offerte</a></li>
+            </ul>
         </div>
-    
-        <ul class="mobile-menu hidden">
-            <li><a href="./index.php">Home</a></li>
-            <?php foreach($templateParams["categorie"] as $categoria): ?>
-            <li>
-                <a href="./index-prodotti-categoria.php?nomeCategoria=<?php echo $categoria["Nome"]?>"><?php echo $categoria["Nome"]; ?></a>
-            </li>
-            <?php endforeach; ?>
-            <li><a href="./index-offerte.php">Offerte</a></li>
-        </ul>
     </nav>
 
-    <!-- SEZIONE PRINCIPALE PER I CONTENUTI -->
-    <main>
+    <!-- Main Content -->
+    <main class="container py-4">
         <?php
-        if(isset($templateParams["nome"])){
+        if (isset($templateParams["nome"])) {
             require($templateParams["nome"]);
         }
         ?>
     </main>
-    
+
     <!-- Footer -->
-    <footer>
-        <ul>
-            <li>All You Knit S.p.A</li>
-            <li>© 1988 All rights reserved</li>
-            <li>Via del Gomitolo 42, 45122 Cesena (FC)</li>
-            <li>Tel. +39 322 8443012</li>
-            <li>Email: info@allyouknit.it</li>
-        </ul>
-        <div class="brand-logo">
-            <img src="./upload/logos/LOGOSCRITTA.png" alt="">
-        </div>
-        <div class="payment-icons">
-            <span><img src="./upload/icons/cc-apple-pay-brands-solid.svg" alt=""></span>
-            <span><img src="./upload/icons/cc-mastercard-brands-solid.svg" alt=""></span>
-            <span><img src="./upload/icons/cc-visa-brands-solid.svg" alt=""></span>
+    <footer class="py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <ul class="list-unstyled">
+                        <li><strong>All You Knit S.p.A</strong></li>
+                        <li>© 1988 All rights reserved</li>
+                        <li>Via del Gomitolo 42, 45122 Cesena (FC)</li>
+                        <li>Tel. +39 322 8443012</li>
+                        <li>Email: info@allyouknit.it</li>
+                    </ul>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="brand-logo">
+                        <img src="./upload/logos/LOGOSCRITTA.png" alt="Logo" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-md-4 text-end">
+                    <div class="payment-icons d-inline-flex gap-2">
+                        <img src="./upload/icons/cc-apple-pay-brands-solid.svg" alt="" width="32">
+                        <img src="./upload/icons/cc-mastercard-brands-solid.svg" alt="" width="32">
+                        <img src="./upload/icons/cc-visa-brands-solid.svg" alt="" width="32">
+                    </div>
+                </div>
+            </div>
         </div>
     </footer>
 </body>

e10a8e7982f07d67b803a1e4207e1a1de0208834
capuanomaurizio <mauricapuano@gmail.com>
2025-01-26 20:06:45 +0100

    Skinny legend

--- utils/edit-product-script.php
+++ utils/edit-product-script.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../db/dbhelper.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "allyouknit", 3306);
-$dbh->editProduct($_POST["product-name"], $_POST["product-price"], $_POST["product-store"], $_POST["product-category"],
+$dbh->editProduct($_POST["product-name"], number_format($_POST["product-price"], 2, ".", ""), $_POST["product-store"], $_POST["product-category"],
                     $_POST["product-color"], $_POST["product-composition"], $_POST["product-tools"], $_POST["product-id"]);
 $titolo = "Modifica di un prodotto del catalogo";
 $contenuto = "Salve cari clienti! Vi scriviamo per comunicarVi che un prodotto del nostro catalogo, ".$_POST["product-name"].", 
